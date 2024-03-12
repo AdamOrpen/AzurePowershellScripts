b532266e7c6bf2234c570b9a3718f6ba2e64c77b
Adam Orpen <Adam.Orpen@microsoft.com>
2024-03-12 13:01:28 +0000

    updated description

--- AzureMonitor/CreateDiagSettings.ps1
+++ AzureMonitor/CreateDiagSettings.ps1
@@ -3,20 +3,20 @@ Param
     [Parameter(Mandatory=$true)]$SubscriptionID,
     [Parameter(Mandatory=$true)]$DiagName
 )    
-###################################################################
-# Author: Adam Orpen                                              #
-# Purpose:  Deploy standard diagnostic setting to all resources   #
-#           in the named subscription                             #
-# Built: 12/3/24                                                  #
-# Tested: 12/3/24                                                 #
-# Language: Powershell                                            #
-# Github: https://github.com/AdamOrpen/AzurePowershellScripts     #
-# This script is an idempotent execution intended to be run       #
-#     multiple times, depending on the status of your estate.     #
-# Please customise following line to set your Workspace target.   #
-###################################################################
+#####################################################################
+# Author: Adam Orpen                                                #
+# Purpose:  Deploy standard diagnostic setting to all resources     #
+#           in the named subscription                               #
+# Built: 12/3/24                                                    #
+# Tested: 12/3/24                                                   #
+# Language: Powershell                                              #
+# Github: https://github.com/AdamOrpen/AzurePowershellScripts       #
+# This script is an idempotent execution intended to be run         #
+#     multiple times, depending on the status of your estate.       #
+# Please customise following line to set your Workspace resource id.#
+#####################################################################
 $WSRID = "/subscriptions/abc/resourceGroups/LAW/providers/Microsoft.OperationalInsights/workspaces/123"
-###################################################################
+#####################################################################
 
 $Sub = Get-AzSubscription -SubscriptionId $SubscriptionID
 Set-AzContext -SubscriptionObject $Sub
